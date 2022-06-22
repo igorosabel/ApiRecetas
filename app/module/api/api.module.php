3095c596346049b200ce044be1b1d988ddab4c71
Iñigo <inigo.gorosabel@gmail.com>
2022-06-22 23:24:19 +0200

    Actualizo OFW (8.0.2 -> 8.0.3)

--- app/module/api/api.module.php
+++ app/module/api/api.module.php
@@ -5,7 +5,7 @@ namespace OsumiFramework\App\Module;
 use OsumiFramework\OFW\Routing\OModule;
 
 #[OModule(
-	actions: 'login, register, getMainData',
+	actions: ['login', 'register', 'getMainData'],
 	type: 'json',
 	prefix: '/api'
 )]
